83eac913d64ce9b7156c82ef3e06149e60bfe525
nishantviswanadha <nishantvishwanadha@gmail.com>
2020-01-24 12:08:33 +0530

    adding dockerfiles

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test1('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
