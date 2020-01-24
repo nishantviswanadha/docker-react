9fc57c7efb7fc7a4b350eba45ad72038daad0c94
nishantviswanadha <nishantvishwanadha@gmail.com>
2020-01-24 12:57:12 +0530

    added doc-1

--- src/App.test.js
+++ src/App.test.js
@@ -7,9 +7,3 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test1('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
